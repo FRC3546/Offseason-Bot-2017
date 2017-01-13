f289189395835a188bc19888be50b3e4817286d3
Mason VanMeurs <masonvanmeurs7@gmail.com>
2017-01-13 16:25:16 -0500

    test commit

--- src/org/usfirst/frc/team3546/robot/RobotMap.java
+++ src/org/usfirst/frc/team3546/robot/RobotMap.java
@@ -32,7 +32,7 @@ public class RobotMap {
 		driveTrainMotors.setExpiration(.1);
 		driveTrainMotors.setSensitivity(.5);
 
-		//max output 0-100%
+		//max output 0-100
 		driveTrainMotors.setMaxOutput(1);
 
 		//inverts the direction of all motors
